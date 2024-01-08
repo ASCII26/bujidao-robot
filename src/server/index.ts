b54040a1de897d897329973f84e122200290a376
musama <musama@tencent.com>
2024-01-08 18:07:34 +0800

    群聊功能开发

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,33 @@
 import Koa from 'koa';
 import 'dotenv/config.js';
-import loggerMiddleware from './middleware/logger';
-import robotMiddleware from './middleware/robot';
+import { format, createLogger, transports} from 'winston';
+import Robot from './robot';
+const { combine, timestamp } = format;
 
 const app = new Koa();
-app.use(loggerMiddleware);
-app.use(robotMiddleware);
+const logger = createLogger({
+  level: 'info',
+  format: combine(
+    timestamp(),
+    format.json(),
+  ),
+  // defaultMeta: { service: 'user-service' },
+  transports: [
+    new transports.File({ dirname: './logs', filename: 'error.log', level: 'error' }),
+    new transports.File({ dirname: './logs', filename: 'combined.log' }),
+    new transports.Console(),
+  ],
+});
+app.use(async (ctx, next) => {
+  if (!ctx.logger) {
+    ctx.logger = logger;
+  }
+  await next();
+});
+
+if (process.env.WECHATY_TOKEN) {
+  new Robot(process.env.WECHATY_TOKEN, logger);
+}
 app.use(ctx => {
   ctx.body = 'Hello Koa';
 });
--- src/server/robot.ts
+++ src/server/robot.ts
@@ -1,20 +1,26 @@
 import {
   Contact,
   Message,
+  Room,
   ScanStatus,
   WechatyBuilder,
-} from 'wechaty';
-import Koa from 'koa';
-import type { WechatyInterface } from 'wechaty/impls';
+  Friendship,
+} from '@juzi/wechaty';
+import { Friendship as FriendshipType } from '@juzi/wechaty-puppet/types' 
+import type { WechatyInterface } from '@juzi/wechaty/impls';
 import qrcodeTerminal from 'qrcode-terminal'
+import winston from 'winston';
+
+const BUJIDAO_NAME = '沐洒布吉岛Robot测试群';
+const ADMINS = ['7881299792907647'];
 
 class Robot {
   private bot: WechatyInterface;
-  constructor(token: string, private ctx: Koa.Context) {
-    console.log('=====create robot:', token, ctx.logger);
+  private bujidaoRoom?: Room;
+  constructor(token: string, private logger: winston.Logger) {
     this.bot = WechatyBuilder.build({
       name: 'bujidao-bot',
-      puppet: 'wechaty-puppet-service',
+      puppet: '@juzi/wechaty-puppet-service',
       puppetOptions: {
         token,
       }
@@ -22,15 +28,46 @@ class Robot {
     this.listen();
   }
 
-  private listen() {
-    this.bot.on('scan', this.onScan)
-    this.bot.on('login', this.onLogin)
-    this.bot.on('logout', this.onLogout)
-    this.bot.on('message', this.onMessage)
+  private async listen() {
+    this.bot.on('scan', this.onScan.bind(this));
+    this.bot.on('login', this.onLogin.bind(this));
+    this.bot.on('logout', this.onLogout.bind(this));
+    this.bot.on('message', this.onMessage.bind(this));
+    this.bot.on('room-join', this.onRoomIn.bind(this));
+    this.bot.on('friendship', this.onFriendship.bind(this));
 
     this.bot.start()
-      .then(() => this.ctx.logger.info('StarterBot', 'Starter Bot Started.'))
-      .catch((e: unknown) => this.ctx.logger.error('StarterBot', e))
+      .then(async () => {
+        this.logger.info('Bot start success.')
+        this.bujidaoRoom = await this.bot.Room.find({ topic: BUJIDAO_NAME });
+      })
+      .catch((e: unknown) => this.logger.error(e))
+  }
+
+  private async onFriendship(friendship: Friendship) {
+    if (friendship.type() === FriendshipType.Receive) {
+      await friendship.accept();
+      if (/群/.test(friendship.hello()) && this.bujidaoRoom) {
+        // if want to send msg , you need to delay sometimes
+        await new Promise(r => setTimeout(r, 1000))
+        
+        const user = friendship.contact();
+        this.inviteToRoom(this.bujidaoRoom, user);
+      }
+      this.logger.info(`Friendship accept success.`);
+      friendship
+    }
+  }
+
+  private async onRoomIn(room: Room, inviteeList: Contact[], inviter: Contact) {
+    const nameList = inviteeList.map(c => c.name()).join(',');
+    const members = await room.memberAll();
+    room.say(`欢迎 ${nameList}！！！\n欢迎成为沐洒布吉岛第${members.length}位岛民！\n可以先看下公告，了解下群规哈～`);
+    this.logger.info({
+      room: room.topic(),
+      invitee: nameList,
+      inviter: inviter.name(),
+    })
   }
 
   private onScan(qrcode: string, status: ScanStatus) {
@@ -39,44 +76,120 @@ class Robot {
         'https://wechaty.js.org/qrcode/',
         encodeURIComponent(qrcode),
       ].join('')
-      this.ctx.logger.info('StarterBot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcodeImageUrl)
-  
+      this.logger.info(`onScan: ${ScanStatus[status]}(${status}) - ${qrcodeImageUrl}`)
       qrcodeTerminal.generate(qrcode, { small: true })  // show qrcode on console
   
     } else {
-      this.ctx.logger.info('StarterBot', 'onScan: %s(%s)', ScanStatus[status], status)
+      this.logger.info(`onScan: ${ScanStatus[status]}(${status})`)
     }
   }
 
   private onLogin (user: Contact) {
-    this.ctx.logger.info('StarterBot', '%s login', user)
+    this.logger.info(`${user} login`)
   }
   
   private onLogout (user: Contact) {
-    this.ctx.logger.info('StarterBot', '%s logout', user)
+    this.logger.info(`${user} logout`)
   }
 
   private async onMessage (msg: Message) {
     if (msg.self()) {
       return
     }
-  
-    this.ctx.logger.info('StarterBot', msg.toString())
-  
-    if (msg.text() === 'ding') {
-      await msg.say('dong')
-    }
-  
-    const room = msg.room()
+
+    const text = msg.text();
+    const talker = msg.talker();
+    const room = msg.room();
+
     if (room) {
-      const topic = await room.topic()
-      console.log(`room topic is : ${topic}`)
+      this.roomMessageHandler({ msg, room, text, talker })
+      return;
     }
-  
-    if (await msg.mentionSelf()) {
-      const text = msg.text()
-      console.log(`被人@了，文本为：${text}`)
-      await msg.say(`请问有什么事吗？你刚刚对我说：(${text})`);
+
+    this.privateMessageHandler(text, talker)
+  }
+
+  // 群聊消息处理
+  private async roomMessageHandler({
+    msg, room, text, talker
+  }: {
+    msg: Message,
+    room: Room,
+    text: string,
+    talker: Contact,
+  }) {
+    if (!msg.mentionSelf()) {
+      return;
+    }
+    const topic = await room.topic();
+    const owner = room.owner();
+
+    this.logger.info({
+      label: '收到群聊消息',
+      message: text,
+      userName: talker.name(),
+      topic,
+    });
+
+    console.log('=====owner, currentUser, talker', owner?.id, this.bot.currentUser.id, talker.id);
+
+    // 踢人关键字
+    if (/踢人/.test(text)) {
+      // 非群主
+      if (!ADMINS.includes(talker.id)) {
+        await msg.say('管理员才有权限执行这个操作')
+      } else {
+        await this.kickHandler(msg, room);
+      }
+      return;
+    }
+
+    // 打印群公告
+    if (/公告/.test(text)) {
+      await msg.say(await room.announce());
+      return;
+    }
+
+    await msg.say(`请问有什么事吗？你刚刚对我说：(${text})`);
+  }
+
+  private async kickHandler(msg: Message, room: Room) {
+    // 执行踢人操作
+    const mentionList = await msg.mentionList();
+    const kickList = mentionList.filter(c => ![...ADMINS, this.bot.currentUser.id].includes(c.id));
+
+    if (kickList.length === 0) {
+      await msg.say('你要踢谁？请先@我，再使用 “踢人 @+群成员” 的格式告诉我');
+      return;
+    }
+    await room.remove(kickList)
+    await msg.say(`已将${kickList.map(c => c.name()).join('，')}踢出`);
+  }
+
+  // 个人消息处理
+  private async privateMessageHandler(text: string, talker: Contact) {
+    const userName = talker.name();
+    this.logger.info({
+      label: '收到个人消息',
+      message: text,
+      userName,
+    });
+
+    // 进群邀请
+    if (/群/.test(text) && this.bujidaoRoom) {
+      this.inviteToRoom(this.bujidaoRoom, talker);
+    }
+  }
+
+  private async inviteToRoom(room: Room, user: Contact) {
+    const userName = user.name();
+    if (await room.has(user)) {
+      await user.say('你已经在群了')
+    } else {
+      await user.say('稍等，我拉你进去');
+      room.add(user)
+        .then(() => this.logger.info({ label: '邀请入群成功', userName }))
+        .catch((error) => this.logger.error({ label: '邀请入群失败', error, userName }))
     }
   }
 }
